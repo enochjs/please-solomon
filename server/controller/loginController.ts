6149b3882e4686d0dee6adbdb0d006904b78382d
enochjs <wangyongdong@dianwoba.com>
2018-08-21 16:27:42 +0800

    feat(master): login add

--- server/controller/loginController.ts
+++ server/controller/loginController.ts
@@ -1,19 +1,24 @@
 import Koa from 'koa'
 import { Logger } from 'winston'
-import { Controller, Get, Post, TYPE, Context, RequestBody } from '../inversifyKoa'
+import { Controller, Get, Post, TYPE, Context, ResponseBody, RequestBody } from '../inversifyKoa'
 import { provideNamed, inject } from '../inversifyKoa/ioc'
-import Config from '../config'
+import AuthDb from '../db/auth'
 
 @provideNamed(TYPE.Controller, 'LoginController')
 @Controller('/')
 export default class LoginController {
 
-  @inject('Config')
-  private config: Config
-
   @inject('logger')
   private logger: Logger
 
+  @inject('AuthDb')
+  private authDb: AuthDb
+
+  /**
+   * login
+   * @param {Koa.Context} ctx
+   * @memberof LoginController
+   */
   @Get('login')
   public async login (
     @Context() ctx: Koa.Context,
@@ -21,6 +26,22 @@ export default class LoginController {
     await ctx.render('login', {
       title: 'home page',
     })
+  }
+
+  @Post('login/check')
+  @ResponseBody
+  public async loginCheck (
+    @RequestBody() username: 'string',
+    @RequestBody() password: 'string',
+    @Context() ctx: Koa.Context,
+  ) {
+    this.logger.info('Post login/check', username, password)
+    const result = await this.authDb.checkUser(username, password)
+    this.logger.info('Post login/check', result)
+    if (result) {
+      ctx.redirect('/')
+    }
+    return false
   }
 
 }
---
+++ server/db/auth.ts
@@ -0,0 +1,41 @@
+import { IMonkManager, id } from 'monk'
+import * as md5 from 'md5'
+import { provide, inject } from '../inversifyKoa/ioc'
+import { filterDecorator } from '../utils/decorators'
+
+export interface ImageParam {
+  name: string,
+  url: string,
+}
+
+@provide('AuthDb')
+export default class AuthDb {
+
+  @inject('Mongodb')
+  private mongodb: IMonkManager
+
+  /**
+   * 用户登录用户名密码匹配
+   * @param {string} username
+   * @param {string} password
+   * @returns
+   * @memberof AuthDb
+   */
+  @filterDecorator({
+    username: { type: 'string', required: true },
+    password: { type: 'string', required: true },
+  })
+  public async checkUser (username: string, password: string) {
+    const authCollection = await this.mongodb.get('auth')
+    const user: any = await authCollection.find({ username })
+    if (!user.length) {
+      throw new Error('用户名不存在！')
+    }
+    if (md5(password) === user[0].password) {
+      return true
+    } else {
+      throw new Error('用户名密码错误！')
+    }
+  }
+
+}
